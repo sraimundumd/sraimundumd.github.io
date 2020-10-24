089eff7ff46ef576fa8768bcf17e5dfbe4fb4984
sraimundumd <60454888+sraimundumd@users.noreply.github.com>
2020-10-24 14:45:28 -0400

    Final

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,5 +1,13 @@
 // You may wish to find an effective randomizer function on MDN.
+function getRandomIntInclusive(min, max) {
+  const min1 = Math.ceil(min);
+  const max1 = Math.floor(max);
+  return Math.floor(Math.random() * (max1 - min1 + 1) + min1);
+}
 
+//function getRandomInt(min, max) {
+ // return Math.floor(Math.random() * Math.floor(max) - Math.floor(min)) + Math.floor(min);
+//}
 function range(int) {
   const arr = [];
   for (let i = 0; i < int; i += 1) {
@@ -8,10 +16,10 @@ function range(int) {
   return arr;
 }
 
-function sortByKey(first, compare, key) {
-  if (first[key] < compare[key]) {
+function sortByKey(a, b, key) {
+  if (a[key] < b[key]) {
     return -1;
-  } if (first[key] > compare[key]) {
+  } if (a[key] > b[key]) {
     return 1;
   }
   return 0;
@@ -38,7 +46,7 @@ document.body.addEventListener('submit', async (e) => {
         return fromServer[numb];
       });
 
-      const back_arr = country_arr.sort((a, b) => sortByKey(first, compare, 'name'));
+      const back_arr = country_arr.sort((a, b) => sortByKey(b, a, 'name'));
       const ul = document.createElement('ul');
       ul.className = 'flex-inner';
       $('form').prepend(ul);
@@ -46,8 +54,8 @@ document.body.addEventListener('submit', async (e) => {
 
       back_arr.forEach((el, i) => {
         const li = document.createElement('li');
-        $(li).append(`<input type="checkbox" value=$(el.code) id=$(el.code) />`);
-        $(li).append(`<label for=$(el.code)>$(el.name)</label>`);
+        $(li).append(`<input type="checkbox" value=${el.code} id=${el.code} />`);
+        $(li).append(`<label for=${el.code}>${el.name}</label>`);
         $(ul).append(li);
       });
     })
